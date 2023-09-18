57fda5e5457bf928a7c1b8f985e5339bcdaf01ca
Mateus Sales <mateus.lins.sales@gmail.com>
2023-09-18 17:43:39 -0300

    fix-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,45 @@
-import DescriptionMap from '@/components/DescriptionMap'
-import Image from 'next/image'
-import Link from 'next/link'
+import DescriptionMap from "@/components/DescriptionMap";
+import Image from "next/image";
+import Link from "next/link";
 
 function SectionHeaderText(props) {
-  const { children } = props
-  return (
-    <h4 className='pb-5 text-xl font-semibold'>{children}</h4>
-  )
-
+  const { children } = props;
+  return <h4 className="pb-5 text-xl font-semibold">{children}</h4>;
 }
 
 export default function Home() {
-  const links = [
-    { name: 'Email', content: 'jamesmca@live.com', link:'https://www.google.com' },
-    { name: 'GitHub', content: 'jamezmca', link:'https://www.google.com' },
-    { name: 'LinkedIn', content: 'jamezmcarthur', link:'https://www.google.com' },
-    { name: 'YouTube', content: 'smoljames', link:'https://www.google.com' }
-  ]
-
-  const skills = [
-    { name: 'JavaScript', content: ['React', 'React Native', 'SvelteKit', 'Node.js'] },
-    { name: 'Python', content: ['Data Analysis', 'Django', 'Flask', 'Data Visualization'] },
-    { name: 'Web', content: ['Sass', 'TailwindCSS', 'Firebase', 'NoSQL', 'SQL', 'Netlify', 'AWS', 'GCP', 'GIT', 'DOCKER', 'Kubernetes'] },
-    { name: 'Design', content: ['Canvas', 'Pixlr', 'Figma'] },
-  ]
-
-  const workHistory = [
-    { name: 'Zetane Systems (2021 - Present)', content: 'Developer,...........' },
-    { name: 'Zetane Systems (2021 - Present)', content: 'Developer,...........' },
-    { name: 'Zetane Systems (2021 - Present)', content: 'Developer,...........' },
-    { name: 'Zetane Systems (2021 - Present)', content: 'Developer,...........' }
-  ]
-
-  const hobbies = [
-    { name: 'Making coding videos', content: 'I really really really really really enjoy programming' },
-    { name: 'Making coding videos', content: 'I really really really really really enjoy programming' },
-    { name: 'Making coding videos', content: 'I really really really really really enjoy programming' },
-    { name: 'Making coding videos', content: 'I really really really really really enjoy programming' },
-  ]
-
   return (
-    <main className='flex flex-col gap-5 p-4 mx-auto max-w-[900px] w-full'>
-      <h2 className='font-bold sm:py-4 md:py-8 text-4xl sm:text-5xl md:text-6xl'>Digital Résumé</h2>
+    <main className="flex flex-col gap-5 p-4 mx-auto max-w-[900px] w-full">
+      <h2 className="font-bold sm:py-4 md:py-8 text-4xl sm:text-5xl md:text-6xl">
+        Digital Résumé
+      </h2>
       <section>
-        <SectionHeaderText>James McArthur - Web Developer and Educator</SectionHeaderText>
-        <DescriptionMap list={links} type={"links"} />
+        <SectionHeaderText>
+          James McArthur - Web Developer and Educator
+        </SectionHeaderText>
+        <DescriptionMap />
       </section>
       <section>
         <SectionHeaderText>Skills</SectionHeaderText>
 
-        <DescriptionMap list={skills} type={"skills"} />
+        <DescriptionMap />
       </section>
       <section>
         <SectionHeaderText>Projects</SectionHeaderText>
 
-        <p className='text-xs sm:text-sm'>Check out my latest <Link href={'/projects'} className='text-sky-400'>projects</Link></p>
+        <p className="text-xs sm:text-sm">
+          Check out my latest{" "}
+          <Link href={"/projects"} className="text-sky-400">
+            projects
+          </Link>
+        </p>
       </section>
-      <section>
-        <SectionHeaderText>Work History</SectionHeaderText>
 
-        <DescriptionMap list={workHistory} type={"work"} />
-      </section>
       <section>
         <SectionHeaderText>Just For Fun</SectionHeaderText>
 
-        <DescriptionMap list={hobbies} type={"hobbies"} />
+        <DescriptionMap />
       </section>
     </main>
-  )
+  );
 }
--- src/components/DescriptionMap.tsx
+++ src/components/DescriptionMap.tsx
@@ -1,26 +1,103 @@
-import React from 'react'
+import React from "react";
 
-export default function DescriptionMap(props) {
-    const { list, type } = props
+export default function DescriptionMap() {
+  const links = [
+    {
+      name: "Email",
+      content: "jamesmca@live.com",
+      link: "https://www.google.com",
+    },
+    { name: "GitHub", content: "jamezmca", link: "https://www.google.com" },
+    {
+      name: "LinkedIn",
+      content: "jamezmcarthur",
+      link: "https://www.google.com",
+    },
+    { name: "YouTube", content: "smoljames", link: "https://www.google.com" },
+  ];
 
-    const contentDisplay = {
-        'hobbies': (obj) => <p>{obj.content}</p>,
-        'work': (obj) => <p>{obj.content}</p>,
-        'skills': (obj) => <p>{obj.content.join(', ')}</p>,
-        'links': (obj) => <a className='text-sky-400 cursor-pointer' target='_blank' href={obj.link}>{obj.content}</a>
-    }
+  const skills = [
+    {
+      name: "JavaScript",
+      content: ["React", "React Native", "SvelteKit", "Node.js"],
+    },
+    {
+      name: "Python",
+      content: ["Data Analysis", "Django", "Flask", "Data Visualization"],
+    },
+    {
+      name: "Web",
+      content: [
+        "Sass",
+        "TailwindCSS",
+        "Firebase",
+        "NoSQL",
+        "SQL",
+        "Netlify",
+        "AWS",
+        "GCP",
+        "GIT",
+        "DOCKER",
+        "Kubernetes",
+      ],
+    },
+    { name: "Design", content: ["Canvas", "Pixlr", "Figma"] },
+  ];
 
-    return (
-        <div className='flex flex-col gap-3'>
-            {list.map((listItem, listIndex) => {
-                return (
-                    <div className='relative group overflow-hidden  flex items-center gap-1.5 flex-wrap text-xs sm:text-sm' key={listIndex}>
-                        <div className='bg-purple-400 duration-300 group-hover:translate-x-full h-[2px] w-full absolute right-full bottom-0'></div>
-                        <p><b>{listItem.name}</b></p>
-                        {contentDisplay[type](listItem)}
-                    </div>
-                )
-            })}
-        </div>
-    )
+  const workHistory = [
+    {
+      name: "Zetane Systems (2021 - Present)",
+      content: "Developer,...........",
+    },
+    {
+      name: "Zetane Systems (2021 - Present)",
+      content: "Developer,...........",
+    },
+    {
+      name: "Zetane Systems (2021 - Present)",
+      content: "Developer,...........",
+    },
+    {
+      name: "Zetane Systems (2021 - Present)",
+      content: "Developer,...........",
+    },
+  ];
+
+  const hobbies = [
+    {
+      name: "Making coding videos",
+      content: "I really really really really really enjoy programming",
+    },
+    {
+      name: "Making coding videos",
+      content: "I really really really really really enjoy programming",
+    },
+    {
+      name: "Making coding videos",
+      content: "I really really really really really enjoy programming",
+    },
+    {
+      name: "Making coding videos",
+      content: "I really really really really really enjoy programming",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-3">
+      {links.map((i, index) => {
+        return (
+          <div
+            className="relative group overflow-hidden  flex items-center gap-1.5 flex-wrap text-xs sm:text-sm"
+            key={index}
+          >
+            <div className="bg-purple-400 duration-300 group-hover:translate-x-full h-[2px] w-full absolute right-full bottom-0"></div>
+            <p>
+              <b>{i.name}</b>
+            </p>
+            {i.content}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
